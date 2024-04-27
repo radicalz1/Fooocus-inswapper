809877cecaa8fe5e51e9261085abef27006dcd0d
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2024-04-28 03:04:33 +0700

    Update webui.py
    Change Cloudflared indentation

--- webui.py
+++ webui.py
@@ -818,19 +818,19 @@ import time
 import socket
 
 def iframe_thread(port):
-while True:
-time.sleep(0.5)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-result = sock.connect_ex(('127.0.0.1', port))
-if result == 0:
-break
-sock.close()
-print("\nFooocus finished loading, trying to launch cloudflared (if it gets stuck here cloudflared is having issues)\n")
-p = subprocess.Popen(["cloudflared", "tunnel", "--url", "http://127.0.0.1:{}".format(port)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-for line in p.stderr:
-l = line.decode()
-if "trycloudflare.com" in l:
-print("This is the URL to access Fooocus:", l[l.find("https"):], end='')
+    while True:
+        time.sleep(0.5)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex(('127.0.0.1', port))
+        if result == 0:
+            break
+        sock.close()
+    print("\nFooocus finished loading, trying to launch cloudflared (if it gets stuck here cloudflared is having issues)\n")
+    p = subprocess.Popen(["cloudflared", "tunnel", "--url", "http://127.0.0.1:{}".format(port)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    for line in p.stderr:
+        l = line.decode()
+        if "trycloudflare.com" in l:
+            print("This is the URL to access Fooocus:", l[l.find("https"):], end='')
 
 port = 7865 # Replace with the port number used by Fooocus
 threading.Thread(target=iframe_thread, daemon=True, args=(port,)).start()
