2183886dd4d5491ac803d29c3b8d52d174e2d84d
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2024-05-01 13:40:57 +0700

    Update webui.py

--- webui.py
+++ webui.py
@@ -116,9 +116,14 @@ with shared.gradio_root:
                                                           height=768, visible=False, elem_classes=['main_view', 'image_gallery'])
                             progress_html = gr.HTML(value=modules.html.make_progress_html(32, 'Progress 32%'), visible=False,
                                                     elem_id='progress-bar', elem_classes='progress-bar')
-                            gallery = gr.Gallery(label='Gallery', show_label=False, object_fit='contain', visible=True, height=768,
+                            with gr.Row():
+                                gallery = gr.Gallery(label='Gallery', show_label=False, object_fit='contain', visible=True, height=768,
                                                  elem_classes=['resizable_area', 'main_view', 'final_gallery', 'image_gallery'],
                                                  elem_id='final_gallery')
+                                glry = gr.Gallery(value='/content/Fooocus-inswapper/imgs', label='Gallery', show_label=False, object_fit='contain', visible=True, height=768,
+                                                 elem_classes=['resizable_area', 'main_view', 'final_gallery', 'image_gallery'],
+                                                 elem_id='final_gallery')
+                            
                         aspect_ratios_selection = gr.Radio(label='Aspect Ratios', choices=modules.config.available_aspect_ratios,
                                    value=modules.config.default_aspect_ratio, info='width × height',
                                    elem_classes='aspect_ratios', scale=3, visible=False)
