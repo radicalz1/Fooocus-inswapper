2b9b72c35d1fd87442c824cf1e645b616be53ccf
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2024-05-09 06:32:10 +0700

    Update face_swap.py

--- modules/face_swap.py
+++ modules/face_swap.py
@@ -25,37 +25,35 @@ def perform_face_swap(images, inswapper_source_image, inswapper_source_image_ind
   checkpoint = torch.load(ckpt_path)["params_ema"]
   codeformer_net.load_state_dict(checkpoint)
   codeformer_net.eval()
-for item in images:
-    for idx, image in enumerate(inswapper_source_image):
-      sim = image
-      sin = inswapper_source_image_indicies[idx]
-      tin = inswapper_target_image_indicies[idx]
-      # source_image = Image.fromarray(inswapper_source_image)
-      print(f"Inswapper: Source indicies: {inswapper_source_image_indicies}")
-      print(f"Inswapper: Target indicies: {inswapper_target_image_indicies}")      
-      result_image = process([sim], item, sin, tin, "../inswapper/checkpoints/inswapper_128.onnx")
-      # result_image = process([source_image], item, inswapper_source_image_indicies, inswapper_target_image_indicies, "../inswapper/checkpoints/inswapper_128.onnx")
-      # swapped_images.append(result_image)
-      print("==================")
-      print("Inswapper Finished")
-      print("Start Restoration")
-      print("==================")
-
-  # if True:
-      
-      result_image = cv2.cvtColor(np.array(result_image), cv2.COLOR_RGB2BGR)
-      result_image = face_restoration(result_image, 
-                                      True, 
-                                      True, 
-                                      1, 
-                                      0.5,
-                                      upsampler,
-                                      codeformer_net,
-                                      device)
-
-      swapped_images.append(result_image)
-      print("===============")
-      print("Done, appended")
-      print("===============")
+  for item in images:
+      for idx, image in enumerate(inswapper_source_image):
+        sim = image
+        sin = inswapper_source_image_indicies[idx]
+        tin = inswapper_target_image_indicies[idx]
+        # source_image = Image.fromarray(inswapper_source_image)
+        print(f"Inswapper: Source indicies: {inswapper_source_image_indicies}")
+        print(f"Inswapper: Target indicies: {inswapper_target_image_indicies}")      
+        result_image = process([sim], item, sin, tin, "../inswapper/checkpoints/inswapper_128.onnx")
+        # result_image = process([source_image], item, inswapper_source_image_indicies, inswapper_target_image_indicies, "../inswapper/checkpoints/inswapper_128.onnx")
+        # swapped_images.append(result_image)
+        print("==================")
+        print("Inswapper Finished")
+        print("Start Restoration")
+        print("==================")
+        
+        result_image = cv2.cvtColor(np.array(result_image), cv2.COLOR_RGB2BGR)
+        result_image = face_restoration(result_image, 
+                                        True, 
+                                        True, 
+                                        1, 
+                                        0.5,
+                                        upsampler,
+                                        codeformer_net,
+                                        device)
   
+        swapped_images.append(result_image)
+        print("===============")
+        print("Done, appended")
+        print("===============")
+    
   return swapped_images
