ce864c6ec3dd69a74a352c0199514c286f97b01b
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2024-05-09 15:15:47 +0700

    Update face_swap.py

--- modules/face_swap.py
+++ modules/face_swap.py
@@ -1,13 +1,26 @@
 import sys
 from PIL import Image
 import numpy as np
+import cv2
 sys.path.append('../inswapper')
 
-from inswapper.swapper import process
+from inswapper.swapper import process, getFaceAnalyser, get_many_faces
 
 def perform_face_swap(images, inswapper_source_image, inswapper_source_image_indicies, inswapper_target_image_indicies):
   swapped_images = []
   swapped_images.extend(images)
+
+  print("==================")
+  print(f"Read target image")
+  target_img = cv2.cvtColor(np.array(target_img), cv2.COLOR_RGB2BGR)
+  print("========================================================")
+  print(f"detect faces that will be replaced in the target image")
+  target_faces = get_many_faces(face_analyser, target_img)
+  num_target_faces = len(target_faces)
+  swapped_images.append(target_faces)
+  print("==================")
+  print(f"Appended Target Faces")
+
   from inswapper.restoration import face_restoration,check_ckpts,set_realesrgan,torch,ARCH_REGISTRY,cv2
   # make sure the ckpts downloaded successfully
   check_ckpts()
@@ -25,20 +38,21 @@ def perform_face_swap(images, inswapper_source_image, inswapper_source_image_ind
   checkpoint = torch.load(ckpt_path)["params_ema"]
   codeformer_net.load_state_dict(checkpoint)
   codeformer_net.eval()
+
   for item in images:
       for idx, image in enumerate(inswapper_source_image):
         sim = image
         sin = inswapper_source_image_indicies[idx]
         tin = inswapper_target_image_indicies[idx]
-        # source_image = Image.fromarray(inswapper_source_image)
         print(f"Inswapper: Source indicies: {inswapper_source_image_indicies}")
         print(f"Inswapper: Target indicies: {inswapper_target_image_indicies}")      
-        result_image = process([sim], item, sin, tin, "../inswapper/checkpoints/inswapper_128.onnx")
+
+        result_image = process(target_faces, num_target_faces, [sim], item, sin, tin, "../inswapper/checkpoints/inswapper_128.onnx")
         # result_image = process([source_image], item, inswapper_source_image_indicies, inswapper_target_image_indicies, "../inswapper/checkpoints/inswapper_128.onnx")
         # swapped_images.append(result_image)
         print("==================")
-        print("Inswapper Finished")
-        print("Start Restoration")
+        print(f"Inswap {idx} Finished")
+        print(f"Start {idx} Restoration")
         print("==================")
         
         result_image = cv2.cvtColor(np.array(result_image), cv2.COLOR_RGB2BGR)
@@ -53,7 +67,7 @@ def perform_face_swap(images, inswapper_source_image, inswapper_source_image_ind
   
         swapped_images.append(result_image)
         print("===============")
-        print("Done, appended")
+        print(f"Done restore and append {idx}")
         print("===============")
     
   return swapped_images
