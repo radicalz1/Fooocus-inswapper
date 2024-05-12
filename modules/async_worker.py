ce9f3bb47d4d27d1a0873f3d20dcca17cfa099f2
radicalz1 <142083579+radicalz1@users.noreply.github.com>
2024-05-12 07:51:31 +0700

    Update async_worker.py

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -1145,7 +1145,7 @@ def worker():
                             bg_im = bg_im.convert("RGBA")
                             fg_im = fg_im.convert("RGBA")
                             # Invert foreground alpha for darkening effect (more alpha = darker)
-                            inverted_alpha = 1.0 - fg_image.split()[-1]
+                            inverted_alpha = 1.0 - fg_im.split()[-1]
                             # Blend the images using weighted addition
                             blended_image = Image.blend(bg_im, fg_im, alpha=alpha)
                             # Apply the inverted alpha to the top layer
